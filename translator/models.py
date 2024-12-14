346337b14060e77719778de7bcefd964ffe41685
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-12-14 10:41:49 +0530

    models created

--- translator/models.py
+++ translator/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class TranslationHistory(models.Model):
+    source_text = models.TextField()
+    target_text = models.TextField()
+    source_language = models.CharField(max_length=10)
+    target_language = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.source_language} -> {self.target_language}: {self.source_text[:50]}"
+        )
